27a1820f70fb103ff32d759daeda310929b7d978
Raj kumar <rajkumarvermacea@gmail.com>
2026-06-30 12:30:29 +0530

    Integarate Clerk authentication by adding Clerk provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
      
        <header>
          <Show when="signed-out">
-           <SignInButton />
-           <SignUpButton />
+           <SignInButton mode='modal'/>
+           <SignUpButton mode='modal'/>
          </Show>
          <Show when="signed-in">
            <UserButton />
