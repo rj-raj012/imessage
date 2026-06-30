911a427208b20b848c975ed61a9fbcf9f24cd85c
Raj kumar <rajkumarvermacea@gmail.com>
2026-06-30 13:59:32 +0530

    feat: add Dockerfile for multi-stage build and .dockerignore for ignored files

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,10 +4,15 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express'
 
+import fs from "fs";
+import path from "path";
+
 const app = express();
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(),"public");
 
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL,credentials:true}))
@@ -16,7 +21,15 @@ app.use(clerkMiddleware())
 app.get("/health",(req,res)=>{
     res.status(200).json({ok:true});
 })
-
+// if the publci deirectory exist , serve the static file
+//this is for the production build
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir))
+    
+    app.get("/{*any}",(req,res,next) =>{
+res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err));
+    }) 
+}
 
 app.listen(PORT, () => {
     connectDB();
